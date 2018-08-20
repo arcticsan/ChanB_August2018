0a8c155ae771dd44ccc63e4375667c8ae0c1e298
APCS2nd <APCS2nd@MHS-Dreyer-01>
2018-08-20 09:46:10 -0700

    First Project To Commit To Github for APCS 2018-2019!

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,15 @@
+
+public class Hello_World 
+{
+	/**
+	 *Prints out a message saying Hello!
+	 * 
+	 * @author Bryan Chan
+	 * @version 8/17/2018
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World! From Bryan Chan");
+		//Display the String
+	}
+}
