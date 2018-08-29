43181ed5adc128c12fa2c8ca52775d84b60d43ee
APCS2nd <APCS2nd@MHS-Dreyer-01>
2018-08-29 11:05:11 -0700

    Revised Comments for Hello World Program.

--- Hello_World/src/Hello_World.java
+++ Hello_World/src/Hello_World.java
@@ -1,12 +1,11 @@
-
+/**
+*Prints out a message saying Hello!
+* 
+* @author Bryan Chan
+* @version 8/17/2018
+*/
 public class Hello_World 
 {
-	/**
-	 *Prints out a message saying Hello!
-	 * 
-	 * @author Bryan Chan
-	 * @version 8/17/2018
-	 */
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World! From Bryan Chan");
