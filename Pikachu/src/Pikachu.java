090826e6ebbe99bfda4f8a02daafe6fc63afe448
APCS2nd <APCS2nd@MHS-Dreyer-01>
2018-08-31 09:51:19 -0700

    Started Pikachu Project

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,13 @@
+/*
+ 
+ */
+public class Pikachu {
+
+	public static void main(String[] args) {
+		System.out.print("    \(\\__/\)");
+		System.out.print("    (o^.^)");
+		System.out.print("  z(_(\")(\")");
+
+	}
+
+}
